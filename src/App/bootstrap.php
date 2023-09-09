ba3d224ad1e21a4083920f9c7491a660e531d335
Dipak Pokharel <Deepakpokharel811@gmail.com>
2023-09-10 02:12:45 +0545

    Initial commit

---
+++ src/App/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Framework\App;
+
+require __DIR__.'/../../vendor/autoload.php';
+
+$App = new App();
+
+return $App;
---
+++ src/Framework/App.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Framework;
+
+class App
+{
+    public function showData()
+    {
+        echo 'I will pass in an interview at my dream company';
+    }
+}
